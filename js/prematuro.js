d9dfe7c77adf14879da1022e5f4ef893fd469ad3
Alejandro Martin Diaz <elprolol999@gmail.com>
2024-02-15 20:52:12 +0100

    priemra parte del scape room

---
+++ js/prematuro.js
@@ -0,0 +1,53 @@
+"use strict"
+
+
+
+let currentQuestion = 1;
+let attempts = 0;
+
+function checkAnswer(correct) {
+    if (correct) {
+        document.getElementById(`question${currentQuestion}`).classList.add('hidden');
+        currentQuestion++;
+        attempts = 0; // Reiniciar el contador de intentos
+        if (currentQuestion <= 7) {
+            document.getElementById(`question${currentQuestion}`).classList.remove('hidden');
+        } else {
+            document.getElementById('githubCat').style.display = 'block';
+            document.getElementById('speechBubble').style.display = 'block';
+            document.getElementById('responseForm').style.display = 'block';
+        }
+    } else {
+        attempts++;
+        if (attempts === 2) {
+            currentQuestion = 1; // Volver a la primera pregunta
+            document.querySelectorAll('.question').forEach(question => {
+                question.classList.add('hidden');
+            });
+            document.getElementById('question1').classList.remove('hidden');
+            attempts = 0; // Reiniciar el contador de intentos
+            alert('Has agotado tus intentos en esta pregunta. Volviendo a la primera pregunta.');
+        } else {
+            alert('Respuesta incorrecta. Por favor, inténtalo de nuevo.');
+        }
+    }
+}
+
+function checkLetter() {
+    const letterInput = document.getElementById('letterInput').value.toLowerCase();
+    if (letterInput === 'b') {
+        alert('¡Respuesta correcta! Desvelando el secreto...\nhttps://github.com');
+        return false;
+    } else {
+        alert('Respuesta incorrecta. Volviendo a la primera pregunta.');
+        currentQuestion = 1; // Reiniciar a la primera pregunta
+        document.querySelectorAll('.question').forEach(question => {
+            question.classList.add('hidden');
+        });
+        document.getElementById('question1').classList.remove('hidden');
+        document.getElementById('githubCat').style.display = 'none'; // Ocultar el gato de GitHub
+        document.getElementById('speechBubble').style.display = 'none'; // Ocultar el bocadillo
+        document.getElementById('responseForm').style.display = 'none'; // Ocultar el formulario de respuesta
+        return false;
+    }
+}
